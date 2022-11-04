e963e62ecf4f16835326af67348679084abe24e5
Carlos Azevedo <cmaz90@gmail.com>
2022-11-04 16:50:35 +0000

    added multi gspn gym env that uses individual robot actions instead of transitions as actions - still work in progress

--- gspn_gym_env/__init__.py
+++ gspn_gym_env/__init__.py
@@ -9,3 +9,8 @@ register(
     id='multi-gspn-env-v0',
     entry_point='gspn_gym_env.envs:MultiGSPNenv',
 )
+
+register(
+    id='multi-gspn-mmdp-env-v0',
+    entry_point='gspn_gym_env.envs:MultiGSPN_MMDPenv',
+)
--- gspn_gym_env/envs/__init__.py
+++ gspn_gym_env/envs/__init__.py
@@ -1,2 +1,3 @@
 from gspn_gym_env.envs.GSPNenv import GSPNenv
 from gspn_gym_env.envs.MultiGSPNenv import MultiGSPNenv
+from gspn_gym_env.envs.MultiGSPN_MMDPenv import MultiGSPN_MMDPenv
